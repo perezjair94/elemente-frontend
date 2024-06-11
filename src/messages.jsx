601dd62ce943b2e4ebd7a4dee9bdfb55969cbfba
Jair Pérez <oquendojair10@gmail.com>
2024-06-11 13:21:07 -0500

    feat(messages): change height value

--- src/messages.jsx
+++ src/messages.jsx
@@ -7,7 +7,7 @@ export default function MessagesPage() {
       <h1 className="mb-4">Messages</h1>
       <div
         id="container"
-        className="h-[calc(100vh-180px)] w-full overflow-y-auto overscroll-auto flex flex-col space-y-2"
+        className="h-[calc(100dvh-180px)] w-full overflow-y-auto overscroll-auto flex flex-col space-y-2"
       >
         <Messages />
       </div>
